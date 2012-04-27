65d554f6d33fe7cef8cf0aea3c829129c36ec754
ovancop <ovan89@gmail.com>
2012-04-28 00:18:01 +0700

    Update file ./func/filter_funct.php dengan penambahan beberapa function

--- func/filter_func.php
+++ func/filter_func.php
@@ -1,4 +1,26 @@
 <?php
+//open connection (belum fix) seharusnya diloading oleh index.php
+include './database.php';
+
+//seleksi combobox apa yang sedang dipilih
+if (isset ($_GET['data'])){ 
+	
+	//get_data(array ('id_field' => $_GET['field']));
+	
+}else if (isset ($_GET['compare'])){ 
+	get_data(array (
+					'kode' => $_GET['kode'],
+					'id_field' => $_GET['field'],
+					'id_compare' => $_GET['id_compare']
+					)
+					);
+}else if (isset ($_GET['field'])){
+	get_compare(array ('id_field' => $_GET['kode']));
+}else if (isset ($_GET['table'])){
+	get_field(array ('id_table' => $_GET['kode']));
+}
+
+//load data pada table view_table
 function get_table(){
 	$query = "SELECT * FROM view_table";
 	$result = mysql_query($query) or die (mysql_error);
@@ -7,5 +29,58 @@ function get_table(){
 			echo "<option value=$data[tableID]>" . $data['tableName']. "</option>";
 		}	
 	}
+}
+
+//load data pada table view_field
+function get_field($dataTable){
+	$query = "SELECT * FROM view_field WHERE ID_Table = '" .$dataTable['id_table']. "'"; //print_r($query);
+	$result = mysql_query($query) or die (mysql_error);
+	if (mysql_num_rows($result)){
+		echo "<option value=''></option>";
+		while ($data = mysql_fetch_array($result)){
+			echo "<option value=$data[ID_Field]>". $data['Field']."</option>";	
+		}
+	} 
+}
+
+//load data pada table view_field untuk field COMPARE
+function get_compare($dataCompare){
+	$query = "SELECT Compares FROM view_field WHERE ID_Field = '". $dataCompare['id_field']."'";//print_r($query);
+	$result = mysql_query($query) or die (mysql_error);
+	if (mysql_num_rows($result)){
+		echo "<option value=''></option>";
+		$data = mysql_fetch_assoc($result);
+		$exp = explode (',', $data['Compares']);
+		
+		foreach ($exp as $value){
+			echo "<option value='".$value."'>" .$value. "</option>";
+		}
+	}
+}
+
+//load data pada table view_table untuk field FromTable
+function get_data($dataData){ 
+	$query = "SELECT * FROM view_field WHERE ID_Field = '".$dataData['id_field']."'"; //print_r($query);
+	$result = mysql_query($query) or die (mysql_error);
+	
+	if (mysql_num_rows($result)){ 
+		$dataTable = mysql_fetch_assoc($result);
+		$field_1 = $dataTable['Get_Field_FromTable_1'];
+		$field_2 = $dataTable['Get_Field_FromTable_2'];
+		
+		if (($dataData['kode'] == '=') or ($dataData['kode'] == '<>')){
+			$getToTable = "SELECT * FROM ". $dataTable[FromTable]. " ORDER BY $field_1 ASC"; //print_r($getToTable);
+		}
+		
+		$resToTable = mysql_query($getToTable) or die (mysql_error);
+		if (mysql_num_rows($resToTable)){ 
+			echo "<option value=''></option>";
+			while ($dataToTable = mysql_fetch_array($resToTable)){ 
+				echo "<option value=$dataToTable[$field_1]>".$dataToTable[$field_2]."</option>";
+			}
+		}
+	}
+	
+	
 }
 ?>
