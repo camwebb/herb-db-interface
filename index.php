f1734618a84f9d5c618610ee407b5d72a2022ae8
Cam Webb <cwebb@oeb.harvard.edu>
2012-04-30 07:20:03 +0800

    This still points to database.php, but you do not commit database.php because it contains password info. database.php should exist on your machine, but not in the repo.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 define('_IBIS', TRUE);
 
-include 'database_template.php'; 
+include 'database.php'; 
 include 'func/references_func.php';
 include 'session_list.php';
 include 'func/filter_func.php';
