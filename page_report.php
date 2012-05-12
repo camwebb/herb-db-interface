80ac24adc91b0e43801b48e83710c5f11b6b96d1
yanriza <aspieyanrieza@gmail.com>
2012-05-12 14:57:21 +0700

    design awal reporting

--- page_report.php
+++ page_report.php
@@ -1,1 +1,353 @@
-page report
+<div style="width:650px;">
+<div style="">
+  
+  <table border="0">
+		<td>
+	  	<fieldset>
+		  	<table border="0" align="center">
+			  	<tr>
+				  	<td style="padding:5px;" align="center"><h4>Herbarium Bogoriense</h4>PUSLIT BIOLOGI - LIPI INDONESIA<hr></td>	
+  		  	</tr>
+
+			  	<tr>
+  					<td align="center"><p><b>Nama Tumbuhan <br/> Nama Jenis</b></p></td>
+  				</tr>
+  			</table><hr>
+
+			<table border="0">
+				<tr>
+					<td>Locality</td>
+					<td>:</td>
+					<td width="242">1</td>
+				</tr>
+				<tr>
+					<td>Latitude</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Longitude</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Altitude</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Habitat</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+			</table><hr>
+
+			<table border="0">
+				<tr>
+					<td>Collector(s)</td>
+					<td>:</td>
+					<td width="100">1</td>
+				</tr>
+				<tr>
+					<td>No.</td>
+					<td>:</td>
+					<td>1</td>
+					<td>Date</td>
+				</tr>
+				<tr>
+					<td>Local Name</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Habit</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Uses</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Determined By</td>
+					<td>:</td>
+					<td>1</td>
+					<td>Date</td>
+				</tr>
+			</table><hr>	
+
+			<table border=0>
+				<tr>
+					<td><p><b>Duplicate Sent to</b></p>Date Printed :</td>
+				</tr>
+				<tr>
+					<td><p><b>Notes</b></p></td>
+				</tr>
+
+    </table>
+    </fieldset>
+		</td>
+
+      <td width="50"></td>
+
+		<td>
+			<fieldset>
+			<table border="0" align="center">
+				<tr>
+					<td style="padding:5px;" align="center"><h4>Herbarium Bogoriense</h4>PUSLIT BIOLOGI - LIPI INDONESIA<hr></td>
+				</tr>
+				<tr>
+					<td align="center"><p><b>Nama Tumbuhan <br/> Nama Jenis</b></p></td>
+				</tr>
+			</table><hr>
+			<table border="0">
+				<tr>
+					<td>Locality</td>
+					<td>:</td>
+					<td width="242">1</td>
+				</tr>
+				<tr>
+					<td>Latitude</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Longitude</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Altitude</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Habitat</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+			</table><hr>
+			<table border="0">
+				<tr>
+					<td>Collector(s)</td>
+					<td>:</td>
+					<td width="100">1</td>
+				</tr>
+				<tr>
+					<td>No.</td>
+					<td>:</td>
+					<td>1</td>
+					<td>Date</td>
+				</tr>
+				<tr>
+					<td>Local Name</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Habit</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Uses</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Determined By</td>
+					<td>:</td>
+					<td>1</td>
+					<td>Date</td>
+				</tr>
+			</table><hr>	
+			<table border=0>
+				<tr>
+					<td><p><b>Duplicate Sent to</b></p>Date Printed :</td>
+				</tr>
+				<tr>
+					<td><p><b>Notes</b></p></td>
+				</tr>
+			</table>
+			</fieldset>
+		</td>
+	</table>
+</div>
+</div>
+<br>
+<div style="width:650px;">
+
+<div style="">
+	<table border="0">
+		<td>
+		<fieldset>
+			<table border="0" align="center">
+				<tr>
+					<td style="padding:5px;" align="center"><h4>Herbarium Bogoriense</h4>PUSLIT BIOLOGI - LIPI INDONESIA<hr></td>
+				</tr>
+				<tr>
+					<td align="center"><p><b>Nama Tumbuhan <br/> Nama Jenis</b></p></td>
+				</tr>
+			</table><hr>
+			<table border="0">
+				<tr>
+					<td>Locality</td>
+					<td>:</td>
+					<td width="242">1</td>
+				</tr>
+				<tr>
+					<td>Latitude</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Longitude</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Altitude</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Habitat</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+			</table><hr>
+			<table border="0">
+				<tr>
+					<td>Collector(s)</td>
+					<td>:</td>
+					<td width="100">1</td>
+				</tr>
+				<tr>
+
+				<td>No.</td>
+					<td>:</td>
+					<td>1</td>
+					<td>Date</td>
+				</tr>
+				<tr>
+					<td>Local Name</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Habit</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Uses</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Determined By</td>
+					<td>:</td>
+					<td>1</td>
+					<td>Date</td>
+				</tr>
+			</table><hr>	
+			<table border=0>
+				<tr>
+					<td><p><b>Duplicate Sent to</b></p>Date Printed :</td>
+				</tr>
+				<tr>
+					<td><p><b>Notes</b></p></td>
+				</tr>
+			</table>
+			</fieldset>
+		</td>
+  <td width="50">
+			
+		</td>
+		<td>
+			<fieldset>
+			<table border="0" align="center">
+				<tr>
+					<td style="padding:5px;" align="center"><h4>Herbarium Bogoriense</h4>PUSLIT BIOLOGI - LIPI INDONESIA<hr></td>
+					
+				</tr>
+				<tr>
+					<td align="center"><p><b>Nama Tumbuhan <br/> Nama Jenis</b></p></td>
+				</tr>
+			</table><hr>
+			<table border="0">
+				<tr>
+					<td>Locality</td>
+					<td>:</td>
+					<td width="242">1</td>
+				</tr>
+				<tr>
+					<td>Latitude</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Longitude</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Altitude</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Habitat</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+			</table><hr>
+			<table border="0">
+				<tr>
+					<td>Collector(s)</td>
+					<td>:</td>
+					<td width="100">1</td>
+				</tr>
+				<tr>
+					<td>No.</td>
+					<td>:</td>
+					<td>1</td>
+					<td>Date</td>
+				</tr>
+				<tr>
+					<td>Local Name</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Habit</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Uses</td>
+					<td>:</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Determined By</td>
+					<td>:</td>
+					<td>1</td>
+					<td>Date</td>
+				</tr>
+			</table><hr>	
+			<table border=0>
+				<tr>
+					<td><p><b>Duplicate Sent to</b></p>Date Printed :</td>
+				</tr>
+				<tr>
+					<td><p><b>Notes</b></p></td>
+				</tr>
+			</table>
+			</fieldset>
+		</td>
+	</table>
+</div>
+</div>
+
